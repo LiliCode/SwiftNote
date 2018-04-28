9b250ce5ed08842dc73f32811f1fea933c7ee72d
Lili <lili2011050508@163.com>
2018-04-28 09:57:33 +0800

    学习控制流语句

---
+++ swift_controlFlow/swift_controlFlow/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  swift_controlFlow
+//
+//  Created by 唯赢 on 2018/4/28.
+//  Copyright © 2018年 李立. All rights reserved.
+//
+
+import Foundation
+
+// # 控制流程语句 #
+
+// for-in 循环
+let books = ["The Swift Programming Language", "数据结构", "高等数学"]
+for bookName in books {
+    print(bookName)
+}
+
+
+
+
+
+
+
+
+
+
+
+
