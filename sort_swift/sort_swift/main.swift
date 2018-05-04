92af42b74c78261a69e417446c4d268de2226cfe
Lili <lili2011050508@163.com>
2018-05-04 11:30:21 +0800

    swift实现快速排序

--- sort_swift/sort_swift/main.swift
+++ sort_swift/sort_swift/main.swift
@@ -8,26 +8,57 @@
 
 import Foundation
 
-let list: [Int] = [2, 6, 1, 8, 3, 10, 5]
+let list: [Int] = [40, 20, 12, 1, 59, 36, 47, 90, 23, 26, 32]
 
-func sort(list: Array<Int>, compre:(Int, Int) -> Bool) -> Array<Int>? {
+func sort(list: Array<Int>) -> Array<Int>? {
     guard !list.isEmpty else {
         return nil
     }
     
     var sortList = Array(list)
-    var i = 0, j = sortList.count - 1   // 下标
-    while i <= j {
-        
-        
-        
-        
-        i += 1
-        j -= 1
-    }
-    
+    let i = 0, j = sortList.count - 1   // 下标
+    // 快排
+    quickSort(list: &sortList, iIndex: i, jIndex: j)
     
     return sortList
+}
+
+func quickSort(list: inout [Int], iIndex: Int, jIndex: Int) -> Void {
+    if iIndex >= jIndex || list.count <= 1 {
+        return  // 递归结束条件
+    }
+    
+    let baseValue = list[iIndex]
+    var i = iIndex, j = jIndex
+    while i < j {
+        // 先让j哨兵先走
+        while i < j {
+            if list[j] < baseValue {
+                break   // 找到比基准数小的数
+            }
+            
+            j -= 1
+        }
+        // i哨兵移动
+        while i < j {
+            if list[i] > baseValue {
+                break   // 找到比基准数大的数
+            }
+            
+            i += 1
+        }
+        
+        list.swapAt(i, j)   // 交换两个数
+    }
+    
+    if i == j { // 两个哨兵相遇了，把相遇的数和基准数交换位置
+        list.swapAt(iIndex, i)
+    }
+    
+//    print("i = \(iIndex) j = \(jIndex)")
+    
+    quickSort(list: &list, iIndex: iIndex, jIndex: i)    // 左
+    quickSort(list: &list, iIndex: i + 1, jIndex: jIndex) // 右
 }
 
 func compre(_ x: Int, _ y: Int) -> Bool {
@@ -36,7 +67,7 @@ func compre(_ x: Int, _ y: Int) -> Bool {
 
 print(list)
 // 开始排序
-if let newList = sort(list: list, compre: compre(_:_:)) {
+if let newList = sort(list: list) {
     print(newList)
 } else {
     print("排序失败")
